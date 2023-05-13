c2a70bd5e05326ac07337106cd2a0a2566eadea2
Alicia Guo <axguo@mit.edu>
2023-05-13 00:02:57 -0400

    gazing grammar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [phrases, setPhrases] = useState([]);
+  const [selectedPhrases, setSelectedPhrases] = useState([]);
+  let RiTa = require("rita");
+
+  const generatePhrases = () => {
+    let newPhrases = [];
+    for (let i = 0; i < 10; i++) {
+      const grammar = {
+        "start": "what if $phrase [3] | $s4 | $s5",
+        "phrase": "$s1 | $s2 | $s3 ",
+        "s1": "$per-pro loved $poss-pro $adj $nn",
+        "s2": "we met at the $adj-phrase $nn",
+        "s3": "things were $adj-phrase",
+        "s4": "a $adj question",
+        "s5": "loving in $adv $vbg ways",
+        "adj-phrase": "$adj | $adj-comp | the $adj-sup | $adv $adj",
+        "adv": RiTa.randomWord({ pos: "rb" }),
+        "vb": RiTa.randomWord({ pos: "vb" }),
+        "vbg": RiTa.randomWord({ pos: "vbg" }),
+        "vbn": RiTa.randomWord({ pos: "vbn" }),
+        "vbz": RiTa.randomWord({ pos: "vbz" }),
+        "nns": RiTa.randomWord({ pos: "nns" }),
+        "nn": RiTa.randomWord({ pos: "nn" }),
+        "adj": RiTa.randomWord({ pos: "jj" }),
+        "adj-comp": RiTa.randomWord({ pos: "jjr" }),
+        "adj-sup": RiTa.randomWord({ pos: "jjs" }),
+        "prep": RiTa.randomWord({ pos: "in" }),
+        "dt": RiTa.randomWord({ pos: "dt" }),
+        "poss-pro": "my | your | our | their | his | her | its",
+        "per-pro": "you | I | we",
+        "det": "another | some | that | this | every | each"
+      };
+      let rg = new RiTa.RiGrammar(grammar);
+      let phrase = rg.expand();
+      newPhrases.push(phrase);
+    }
+    setPhrases(newPhrases);
+  };
+
+  const togglePhraseSelection = (phrase) => {
+    if (selectedPhrases.includes(phrase)) {
+      setSelectedPhrases((prev) => prev.filter((item) => item !== phrase));
+    } else {
+      setSelectedPhrases((prev) => [...prev, phrase]);
+    }
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(JSON.stringify(selectedPhrases));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"app"}>
+      <button  onClick={generatePhrases}>
+        Generate Phrases
+      </button>
+      <ul>
+        {phrases.map((phrase, i) => (
+          <li key={i}>
+            <button
+              className={selectedPhrases.includes(phrase) ? "activeItem" : "listItem"}
+              onClick={() => togglePhraseSelection(phrase)}
+            >
+              {phrase}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <h3>Selected Phrases [{selectedPhrases.length}]:</h3>
+        <p>{JSON.stringify(selectedPhrases)}</p>
+      </div>
+      <button onClick={copyToClipboard}>
+        Copy to Clipboard
+      </button>
     </div>
   );
-}
+};
 
 export default App;
